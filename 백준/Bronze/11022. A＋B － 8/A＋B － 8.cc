01b516ee3c6e0d7bf6fb1d4ad8082e06d633a11e
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2025-03-13 20:38:20 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int num;
+    cin >> num;
+    
+    for(int i=0; i<num; i++){
+        int a, b;
+        cin>>a>>b;
+        cout<< "Case #" << i+1 << ": " << a << " + " << b << " = " << a+b <<"\n";
+    }
+    
+    return 0;
+}

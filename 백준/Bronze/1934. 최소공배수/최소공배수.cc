41906bb3b8ebacf7a3dbc782934a67f29d98d941
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2025-04-08 22:39:53 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int Gcd(int a, int b)
+{
+    int remain = a % b;
+
+    if (remain == 0)
+        return b;
+    else
+        return Gcd(b, remain);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int size, n1, n2;
+
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> n1 >> n2;
+        cout << (n1 * n2) / Gcd(n1, n2) << "\n";
+    }
+
+    return 0;
+}

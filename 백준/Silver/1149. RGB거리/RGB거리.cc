1a6586b1658f341b71f2083d77b895a282c977bd
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2025-05-26 16:27:31 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -3,9 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-int cost[1000][3];
-int dp[1000][3];
-
 int main() {
 
     ios_base::sync_with_stdio(false);
@@ -14,26 +11,24 @@ int main() {
     int n;
     cin >> n;
 
+    int prev[3] = { 0, 0, 0 };
+
     for (int i = 0; i < n; i++)
     {
+        int cost[3];
+        cin >> cost[0] >> cost[1] >> cost[2];
+
+        int curr[3];
+        
+        curr[0] = min(prev[1], prev[2]) + cost[0];
+        curr[1] = min(prev[0], prev[2]) + cost[1];
+        curr[2] = min(prev[0], prev[1]) + cost[2];
+
         for (int j = 0; j < 3; j++)
-        {
-            cin >> cost[i][j];
-        }
+            prev[j] = curr[j];
     }
 
-    dp[0][0] = cost[0][0];
-    dp[0][1] = cost[0][1];
-    dp[0][2] = cost[0][2];
-
-    for (int i = 1; i < n; i++)
-    {
-        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
-    }
-
-    cout << min(min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
+    cout << min({ prev[0], prev[1], prev[2] });
 
     return 0;
 }

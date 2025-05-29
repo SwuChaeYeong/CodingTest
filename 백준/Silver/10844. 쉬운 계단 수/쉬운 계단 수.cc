d65609583235312091be2e169994a9fc957ef46d
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2025-05-29 18:41:14 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int dp[101][10];
 const int mod = 1000000000;
 
 int main() {
@@ -12,26 +11,36 @@ int main() {
     int n;
     cin >> n;
 
+    int prev[10] = { 0 };
+    int curr[10] = { 0 };
+
     for (int i = 1; i <= 9; i++)
     {
-        dp[1][i] = 1;
+        prev[i] = 1;
     }
 
     for (int i = 2; i <= n; i++)
     {
         for (int j = 0; j <= 9; j++)
         {
-            if (j > 0) dp[i][j] += dp[i - 1][j - 1];
-            if (j < 9) dp[i][j] += dp[i - 1][j + 1];
+            curr[j] = 0;
 
-            dp[i][j] %= mod;
+            if (j > 0) curr[j] += prev[j - 1];
+            if (j < 9) curr[j] += prev[j + 1];
+
+            curr[j] %= mod;
+        }
+
+        for (int j = 0; j <= 9; j++)
+        {
+            prev[j] = curr[j];
         }
     }
 
     int result = 0;
     for (int i = 0; i <= 9; i++)
     {
-        result = (result + dp[n][i]) % mod;
+        result = (result + prev[i]) % mod;
     }
 
     cout << result;

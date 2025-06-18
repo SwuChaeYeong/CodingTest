d9d37487ecf5cecc46b6db371abc0aa5ed752fc4
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2025-06-18 19:28:13 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,22 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sstream>
 using namespace std;
-
-vector<string> split(const string& s, char d)
-{
-    vector<string> pieces;
-    string piece;
-    stringstream ss(s);
-
-    while (getline(ss, piece, d))
-    {
-        pieces.push_back(piece);
-    }
-
-    return pieces;
-}
 
 int main() {
 
@@ -26,25 +11,33 @@ int main() {
     string str;
     cin >> str;
 
-    vector<string> minusSplit = split(str, '-');
+    int result = 0, num = 0;
+    bool isMinus = false;
 
-    int result = 0;
-
-    for (int i = 0; i < minusSplit.size(); i++)
+    for (int i = 0; i <= str.size(); i++)
     {
-        int temp = 0;
-
-        vector<string> plusSplit = split(minusSplit[i], '+');
-
-        for (int j = 0; j < plusSplit.size(); j++)
+        // -, +, 문자열 끝인 경우 지금까지 나온 숫자를 결과에 반영
+        if (i == str.size() || str[i] == '+' || str[i] == '-')
         {
-            temp += stoi(plusSplit[j]);
-        }
+            // 마이너스 이후면 빼고
+            if (isMinus)
+                result -= num;
+            // 그 전이면 더함
+            else
+                result += num;
 
-        if (i == 0)
-            result += temp;
+            // 다음 숫자 계산을 위해 초기화
+            num = 0;
+
+            // -를 만났다면 변수를 true로 바꿔줌
+            if (i < str.size() && str[i] == '-')
+                isMinus = true;
+        }
+        // 문자가 숫자인 경우 num에 자리수별로 누적
         else
-            result -= temp;
+        {
+            num = num * 10 + (str[i] - '0');
+        }
     }
 
     cout << result;

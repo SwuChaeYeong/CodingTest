9fc2fe4acd6ca26c798aac8d18d38a628bde8df1
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2025-06-20 17:46:44 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+long long A, B, C;
+
+int mul(long long a, long long b)
+{
+    if (b == 0)
+        return 1;
+
+    long long half = mul(a, b / 2);
+    long long result = (half * half) % C;
+
+    // 짝수면 반으로 바로 나눠지니까 그냥 반환하면 됨
+    if (b % 2 == 0)
+        return result;
+
+    // 홀수면 한 번 더 해줌
+    else
+        return (result * a) % C;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+
+    cin >> A >> B >> C;
+
+    cout << mul(A, B) % C;
+
+    return 0;
+}

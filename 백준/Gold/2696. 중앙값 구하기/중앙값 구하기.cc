c6fd5f1d4c3fc7a7c92b7357e62253864f35bc79
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2025-07-15 17:54:17 +0900

    [Gold II] Title: 중앙값 구하기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        priority_queue<int> maxHeap;
+        priority_queue<int, vector<int>, greater<>> minHeap;
+
+        int M, x, cnt = 0;
+        cin >> M;
+
+        cout << (M + 1) / 2 << '\n';
+
+        for (int i = 0; i < M; i++)
+        {
+            cin >> x;
+
+            // 일단 최대 힙에 삽입
+            if (maxHeap.empty() || x <= maxHeap.top())
+                maxHeap.push(x);
+            else
+                minHeap.push(x);
+
+            // 힙 크기 조절
+            if (maxHeap.size() > minHeap.size() + 1)
+            {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+            }
+            else if (minHeap.size() > maxHeap.size())
+            {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+
+            if (i % 2 == 0)
+            {
+                cnt++;
+                cout << maxHeap.top() << " ";
+
+                if (cnt % 10 == 0)
+                    cout << '\n';
+            }         
+        }
+        cout << '\n';
+    }
+      
+    return 0;
+}

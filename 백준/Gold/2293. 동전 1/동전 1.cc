16b4587b3388548466b45eda62024757f35a5ccc
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2025-10-03 17:53:23 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -4,10 +4,6 @@
 
 using namespace std;
 
-// 추는 최대 500g, 30개니까 최대 무게 차이는 15000
-const int MAX_WEIGHT = 15000;
-// i번째 추까지 고려했을 때 w를 만들 수 있는지
-bool dp[31][MAX_WEIGHT + 1];
 
 int main()
 {
@@ -21,23 +17,21 @@ int main()
     vector<int> coins(n);
 
     // dp[i] = i원을 만드는 경우의 수
-    vector<int> dp(k + 1);
+    vector<int> dp(k + 1, 0);
 
     for (int i = 0; i < n; i++)
     {
         cin >> coins[i];
-
-        // 초기화
-        // dp[coins[i]] = 1;
     }
 
+    // dp[0] = 아무 동전도 사용하지 않고 0원을 만드는 경우 1가지
     dp[0] = 1;
 
     for (int coin : coins)
     {
         for (int i = coin; i <= k; i++)
         {
-            dp[i] = dp[i] + dp[i - coin];
+            dp[i] += dp[i - coin];
         }
     }
     

e90e6d69fa3e29b971a1307ed3d4e908e6c140d0
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2026-02-09 14:11:53 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -1,9 +1,14 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    int N, K;
-    cin >> N >> K;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+
+    int N;
+    cin >> N;
 
     for (int i = 0; i < N; i++)
         cout << 'a';

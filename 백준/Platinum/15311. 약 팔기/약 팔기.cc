e471c65bd130823d9636b2ecf916ba834931302c
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2026-02-25 12:50:56 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N; // 입력은 주어지지만 사실 항상 1,000,000
+
+    cout << 2000 << "\n";
+
+    // 1을 1000개
+    for (int i = 0; i < 1000; ++i)
+        cout << 1 << " ";
+
+    // 1000을 1000개
+    for (int i = 0; i < 1000; ++i)
+        cout << 1000 << " ";
+
+    return 0;
+}

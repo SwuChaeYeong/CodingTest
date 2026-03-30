b8b453710d92b7fd788bb4944daaafea51b303fe
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2026-03-30 20:58:34 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    
+    int answer = 0;
+    
+    int sum = 0;
+    int mul = 1;
+    
+    for (int i = 0; i < num_list.size(); i++)
+    {
+        sum += num_list[i];
+        mul *= num_list[i];
+    }
+    
+    if (mul > sum * sum)
+        answer = 0;
+    
+    else
+        answer = 1;
+    
+    return answer;
+}

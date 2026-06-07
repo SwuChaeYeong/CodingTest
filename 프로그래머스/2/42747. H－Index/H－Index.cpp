e0edd15d24cac302d9a99797f25e7a8e7b27127f
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2026-06-08 00:53:23 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,9 +1,16 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
-    return answer;
+    
+    sort(citations.begin(), citations.end(), greater<int>());
+    
+    for (int i = citations.size() - 1; i >= 0; i--)
+    {
+        if (citations[i] >= i + 1)
+            return i + 1;
+    }
 }

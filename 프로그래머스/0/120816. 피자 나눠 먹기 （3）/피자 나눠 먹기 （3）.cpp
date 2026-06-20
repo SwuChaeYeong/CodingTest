d4ef827fb32e2579d03c1ede48f9952d026b09f6
한채영 <109707083+SwuChaeYeong@users.noreply.github.com>
2026-06-20 21:24:28 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int slice, int n) {
+    
+    if (n % slice != 0)
+    {
+        return n / slice + 1;
+    }
+    else
+    {
+        return n / slice;
+    }
+}
